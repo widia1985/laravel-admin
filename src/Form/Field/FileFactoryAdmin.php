73248e7b2a59071cf5e3d95a68392de8e89b0684
Widia <42551207+widia1985@users.noreply.github.com>
2026-05-07 14:57:54 +0800

    Create FileFactoryAdmin.php

---
+++ src/Form/Field/FileFactoryAdmin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Encore\FactoryAdmin\Forms\Field;
+
+use Encore\Admin\Form\Field;
+use Encore\Admin\Extensions\Tools\ModalField;
+use Encore\Admin\Form\Field\File;
+
+class FileFactoryAdmin extends File
+{
+	protected function initStorage()
+    {
+        $this->disk(config('factoryadmin.upload.disk'));
+    }
+}
