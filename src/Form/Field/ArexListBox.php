9cf10d4219c14cfa4525874e9f86fb26ead4cb33
Widia <42551207+widia1985@users.noreply.github.com>
2026-05-07 14:54:14 +0800

    Create ArexListBox.php

---
+++ src/Form/Field/ArexListBox.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Encore\Admin\Forms\Field;
+
+use Encore\Admin\Form\Field\Listbox;
+
+class ArexListBox extends Listbox
+{
+	public function fill($data)
+    {
+        $this->data = $data;
+		$this->view ='admin.form.listbox';
+
+        if (is_array($this->column)) {
+            foreach ($this->column as $key => $column) {
+                $this->value[$key] = \Illuminate\Support\Arr::get($data, $column);
+            }
+
+            return;
+        }
+
+        if(strpos($this->column,"_KEY")){
+			$key = substr($this->column,0,strpos($this->column,"_KEY"));
+		}
+		else{
+			$key = $this->column;
+		}
+	
+        $this->value = \Illuminate\Support\Arr::get($data, $key);
+
+        $this->formatValue();
+    }
+}
